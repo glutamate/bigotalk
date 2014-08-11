371a831030f3fb6fe1b29a44bcd5b7e0485533aa
Tom Nielsen <tomn@brix>
2014-08-11 21:20:10 +0100

    better plots

--- BigO.hs
+++ BigO.hs
@@ -20,7 +20,7 @@ import Data.Complex
 dt = 0.001
 tmax = 1
 
-per = 175
+per = 300
 amp = 3
 sinf t = amp * sin (per*t/(2*pi))
 
@@ -34,13 +34,16 @@ main = do
   wf <- sampleIO $ mapM (`normal` 0.5) sins
   let v = V.fromList $ map (:+0) wf
   fd <- fft v
-  let real_fd = map realPart $ V.toList fd
-      imag_fd = map imagPart $ V.toList fd
+  let psd = V.map (logBase 10) $ halveFirstAndLast $ V.map ((*2) . (^2) . magnitude) $ firstHalf fd
   T.writeFile "/tmp/bigo.html" $ inRadian $ do
          scatterPlot 400 600 "sins" $ zip ts wf
-         scatterPlot 400 600 "reals" $ zip [0..] real_fd
-         scatterPlot 400 600 "imags" $ zip [0..] imag_fd
+         scatterPlot 400 600 "psd" $ zip [0..] $ V.toList psd
   putStrLn "hello world"
+
+firstHalf v = V.take ((V.length v `div` 2) +1 ) v
+
+halveFirstAndLast v = let last  = V.length v-1 
+                      in v V.// [(0, (v V.! 0)/2), (last, (v V.! last)/2)]
 
 
 inRadian :: H.Html -> T.Text
